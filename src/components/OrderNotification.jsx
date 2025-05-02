37dcae46d7f92f139982135193335c5103072e9c
Mag <166319982+magali-la@users.noreply.github.com>
2025-05-02 16:27:32 -0400

    refined middle section of notification tile for better responsiveness. reformatted grid layout, adjusted padding on smaller screens, hid border on left section for smaller screens preventing overlap, added text wrapping, aligned price to right

--- src/components/OrderNotification.jsx
+++ src/components/OrderNotification.jsx
@@ -20,23 +20,23 @@ const OrderNotification = ({orderStatus, orderTime, customerName, orderName, ord
         switch(orderStatus.toLowerCase()) {
             case 'new':
                 return (
-                    <div className='w-full grid grid-cols-2 justify-start ml-12 items-center'>
-                        <p className='text-n-n1 font-semibold truncate'>{orderName} ({orderQuantity}x) order for {customerName} </p>
-                        <p className='font-medium text-n-n2 ml-20'>Total: £{orderPrice}</p>
+                    <div className='w-full grid grid-cols-5 items-center'>
+                        <p className='w-full col-span-4 pr-2 sm:break-normal text-n-n1 font-semibold'>{orderName} ({orderQuantity}x) order for {customerName} </p>
+                        <p className='w-full text-right font-medium text-n-n2'>Total: £{orderPrice}</p>
                     </div>
                 )
             case 'ongoing':
                 return (
-                    <div className='w-full grid grid-cols-2 justify-start ml-12 items-center'>
-                        <p className='text-n-n1 font-semibold truncate'>{orderName} ({orderQuantity}x) in progress for {customerName}</p>
-                        <p className='font-medium text-n-n2 ml-20'>Total: £{orderPrice}</p>
+                    <div className='w-full grid grid-cols-5 items-center'>
+                        <p className='w-full col-span-4 ml-2 pr-2 sm:break-normal text-n-n1 font-semibold'>{orderName} ({orderQuantity}x) in progress for {customerName}</p>
+                        <p className='w-full text-right font-medium text-n-n2'>Total: £{orderPrice}</p>
                     </div>
                 )
             case 'delivered':
                 return (
-                    <div className='w-full grid grid-cols-2 justify-start items-center ml-12'>
-                        <p className='text-n-n1 font-semibold'>{customerName}'s order was delivered at: {deliveryTime}</p>
-                        <p className='font-medium text-n-n2 ml-20'>Total: £{orderPrice}</p>
+                    <div className='w-full grid grid-cols-5 items-center'>
+                        <p className='w-full col-span-4 ml-2 pr-2 sm:break-normal text-n-n1 font-semibold'>{customerName}'s order was delivered at: {deliveryTime}</p>
+                        <p className='w-full text-right font-medium text-n-n2'>Total: £{orderPrice}</p>
                     </div>
                 );
                 default:
@@ -48,9 +48,9 @@ const OrderNotification = ({orderStatus, orderTime, customerName, orderName, ord
     return(
         <div className='w-full flex gap-4'>
             {/* parent div of the tile itself */}
-            <div className="w-full grid grid-cols-4 items-center px-4 py-3 min-h-16 max-h-auto rounded-md border border-n-n3 bg-notif hover:border-p-button">
+            <div className="w-full grid grid-cols-4 items-center px-4 py-3 min-h-16 max-h-[160px] rounded-md border border-n-n3 bg-notif hover:border-p-button">
                 {/* left section - icon, order status, time, and number */}
-                <div className='flex items-center py-3 border-r-2 border-r-p-button'>
+                <div className='flex items-center py-3 px-2 sm:border-r-2 sm:border-r-p-button'>
                     {/* icon div */}
                     <div className='shrink-0 laptop:mr-6 tablet:mr-5 mr-4'>
                         <img src={ActiveIcon} className='size-2.5'/>
@@ -63,7 +63,7 @@ const OrderNotification = ({orderStatus, orderTime, customerName, orderName, ord
                     </div>
                 </div>
                 {/* main order info content */}
-                <div className='col-span-2'>
+                <div className='col-span-2 px-4'>
                     {displayOrderInfo()}                    
                 </div>
                 {/* view order button section */}
