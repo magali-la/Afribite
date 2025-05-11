70a567316d8d76de82b1bc0d00102a6306dec0f0
Mag <166319982+magali-la@users.noreply.github.com>
2025-05-11 15:38:40 -0400

    feat: search functionality with firestore queries, case insensitive matching

--- src/components/OrderSearch.jsx
+++ src/components/OrderSearch.jsx
@@ -1,10 +1,23 @@
 import { CiSearch } from "react-icons/ci";
 
-const OrderSearch = () => {
+const OrderSearch = ({setSearchTerm, handleSearch}) => {
     return(
         <div className="flex flex-row items-center bg-inherit border border-[#E2725B]/20 focus:outline-none focus:border-[#E2725B] rounded-3xl">
           <CiSearch className='text-2xl text-n-n3 w-1/4'/>
-          <input type="text" className="w-3/4 bg-inherit border-none" placeholder='Seach for Orders'/>
+          <input type="text" className="w-3/4 bg-inherit border-none" placeholder='Search for Orders' 
+          onKeyDown={e => {if (e.key == "Enter" && e.target.value !== ''){
+              console.log('enter pressed');
+
+              let searchValue = e.target.value;
+
+              // update the state of searchterm with the input value
+              setSearchTerm(searchValue);
+
+              // trigger the search function
+              handleSearch(searchValue);
+            }}
+          } 
+          />
             
         </div>
     );
--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { getFirestore, collection, getDocs} from "@firebase/firestore";
+import { getFirestore, collection, query, where, or, getDocs} from "@firebase/firestore";
 import OrderSearch from "../components/OrderSearch.jsx";
 import OrderTabs from "../components/OrderTabs.jsx";
 
 function Notifications() {
   const [orders, setOrders] = useState([]);
+  // use states for search function
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
+
+  // define as global variables
+  const db = getFirestore();
+  const ordersRef = collection(db, "orders");
 
   // function that fetches orders from Firestore
   const getOrderData = async () => {
-    const db = getFirestore();
-    const ordersRef = collection(db, "orders");
     const ordersSnapshot = await getDocs(ordersRef);
 
     // build an array with the orders data
@@ -30,13 +36,69 @@ function Notifications() {
 
   console.log("orders before going to order tab:", orders);
 
+  // function that handles search when term is passed from search bar
+  const handleSearch = async (term) => {
+    let searchValue = term || searchTerm;
+
+    // update isSearching state to true
+    setIsSearching(true);
+    console.log(`Search initialized for '${searchValue}'`);
+
+    // convert search term to title case for case insensitive search
+    function toTitleCase(string) {
+      // take the searchValue, split it into an array by spaces
+      let convertedSearchValue = string.split(' ');
+
+      // take the first character of each element using map convert it to uppercase then add the rest as lowercase
+      convertedSearchValue = convertedSearchValue.map(element => element.charAt(0).toUpperCase() + element.slice(1).toLowerCase());
+      console.log(convertedSearchValue);
+
+      // then join it back 
+      convertedSearchValue = convertedSearchValue.join(' ');
+
+      return convertedSearchValue;
+    }
+
+    let titleCaseSearchValue = toTitleCase(searchValue);
+    console.log(titleCaseSearchValue);
+
+
+    // set up OR query with where conditions 
+    let q = query(ordersRef, 
+        or(where('orderNumber', '==', searchValue),
+            where('customerName', '==', searchValue),
+            where('customerName', '==', titleCaseSearchValue)
+          )
+    )
+    console.log(`Checking for matches for '${searchValue}'`);
+
+    // fetch the snapshot of the orders matching the case
+    const querySnapshot = await getDocs(q);
+    const resultsList = querySnapshot.docs.map((doc) => ({id:doc.id, ...doc.data(), }));
+
+    // sort the results list descending for customer searches
+    resultsList.sort((a, b) => b.orderNumber - a.orderNumber);
+    console.log("Matches found:", resultsList);
+
+    // update the search results state with the matching orders
+    setSearchResults(resultsList);
+  };
+
+  // function that handles clearing the search
+  const clearSearch = () => {
+
+  };
+
   return (
     <div className="bg-eggshell rounded-lg p-4 flex flex-col w-full min-h-svh gap-8">
       {/* title and search */}
       <div className="flex flex-row">
         <div className="flex justify-between w-full">
           <h1 className="text-4xl text-terra-cotta">Order Notifications</h1>
-          <OrderSearch/>
+          <OrderSearch
+            setSearchTerm={setSearchTerm}
+            handleSearch={handleSearch}
+          />
         </div>
       </div>
       {/* tabs and notificatons */}
